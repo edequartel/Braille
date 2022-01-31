47840365380c8424e0ad1e4859081217fe3a0cde
Eric de Quartel <edequartel@protonmail.com>
2022-01-31 10:06:17 +0100

    Initial Commit

---
+++ Braille/BrailleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrailleApp.swift
+//  Braille
+//
+//  Created by Eric de Quartel on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrailleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Braille/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Braille
+//
+//  Created by Eric de Quartel on 24/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color=
+        VStack(){
+        HStack(){
+            Text("Top")
+            Text("Left")
+            Text("Right")
+        }
+        Spacer()
+        HStack(spacing: 10) {
+            VStack(){
+            let name="Eric"
+            Text("Hello, Braille!")
+            Text(name)
+            Spacer()
+            Text("""
+            braille een programma
+            om braille te leren
+            op je iphone,
+            zet eerst voiceover aan
+            en verbind de leesregel
+            via bluetooth
+            """)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Spacer()
+            Text("World")
+            Text("Africa")
+            }
+        }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
